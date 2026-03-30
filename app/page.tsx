de0a95ac915cb4f4f14a2c59a9fcfbf67a4ef879
Lucas Lamar <lucaslamar@Lucass-MacBook-Pro.local>
2026-03-30 16:53:14 -0500

    deployment test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className={`${jersey10.className} text-8xl`}>OH.PAKE!</h1>
-      <p className="text-zinc-600 dark:text-zinc-400">site coming soon</p>
+      <p className="text-zinc-600 dark:text-zinc-400">site coming soon test</p>
     </div>
   );
 }
